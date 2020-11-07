c10878fa323c8ca93d2ee5133aef687444a0a7b0
Ben <25BenjaminH@students.tas.tw>
2020-11-07 11:32:17 +0800

    create post location data structure

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255) #location name
+
+class Post(models.Model):
+    subject = models.CharField(max_length=255) #title
+    content = models.CharField(max_length=255) #content
+    author = models.CharField(max_length=20) #author
+    create_date = models.DateField(default=timezone.now)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
