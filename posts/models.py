689e949ddf4ae4b17d3ad3fbe75388a1c4a57eca
Ben <25BenjaminH@students.tas.tw>
2020-11-14 11:38:48 +0800

    build back stage

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,15 @@ from django.utils import timezone
 class Location(models.Model):
     name = models.CharField(max_length=255) #location name
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     subject = models.CharField(max_length=255) #title
     content = models.CharField(max_length=255) #content
     author = models.CharField(max_length=20) #author
     create_date = models.DateField(default=timezone.now)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject, self.content, self.author, self.create_date, self.location
