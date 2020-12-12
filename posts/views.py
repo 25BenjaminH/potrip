1200bb8add625189fe0c4c03f085de74df8a4e7a
Ben <25BenjaminH@students.tas.tw>
2020-12-12 11:27:43 +0800

    pixnet bootstrap practice

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from .models import Post
+import requests
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    #posts = Post.objects.all()
     #SELECT * FROM posts_post
-    return render(request,'posts/index.html',{'posts':posts})
+    #return render(request,'posts/index.html',{'posts':posts})
 
-# def index2(request):
-#     post = Post.objects.filter(location='汐止地區')
-#     #SELECT * FROM posts_post WHERE location="汐止地區"
-#     return HttpResponse(post)
+    response = requests.get('https://emma.pixnet.cc/mainpage/blog/categories/hot/28')
+    articles = response.json()['articles']
+    return render(request,'posts/index.html',{'articles':articles})
